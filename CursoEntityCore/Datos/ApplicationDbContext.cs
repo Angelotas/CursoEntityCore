c32b37604622f1088ecad6654ee3bf4cc3cd1987
Angel Martin <angel.martin@babel.es>
2022-08-02 20:40:43 +0200

    Creación proyecto MVC basado en Net6. Incluir conexión a BBDD SQL Server.

---
+++ CursoEntityCore/Datos/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoEntityCore.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        // Incluir modelos
+    }
+}
