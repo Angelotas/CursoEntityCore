346dca38c0d5698c09cbce42a910453489e585d4
Angel Martin <angel.martin@babel.es>
2022-08-03 19:52:33 +0200

    Creación de primer modelo Categoria

--- CursoEntityCore/Datos/ApplicationDbContext.cs
+++ CursoEntityCore/Datos/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CursoEntityCore.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoEntityCore.Datos
 {
@@ -10,5 +11,6 @@ namespace CursoEntityCore.Datos
         }
 
         // Incluir modelos
+        public DbSet<Categoria> Categoria { get; set; }
     }
 }
