b27672ecda4845f8cc27545962f32907c59db5f5
Angel Martin <angel.martin@babel.es>
2022-08-05 01:45:38 +0200

    Nueva Vista y Controller Categorias para visualizar los datos en formato tabla

---
+++ CursoEntityCore/Controllers/CategoriasController.cs
@@ -0,0 +1,20 @@
+﻿using CursoEntityCore.Datos;
+using CursoEntityCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoEntityCore.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+        public CategoriasController(ApplicationDbContext contexto)
+        {
+            _context = contexto;
+        }
+        public IActionResult Index()
+        {
+            List<Categoria> listaCategorias = _context.Categoria.ToList();
+            return View(listaCategorias);
+        }
+    }
+}
