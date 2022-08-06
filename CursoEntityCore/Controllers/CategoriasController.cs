0f815dfab3bd1e0003c42183c34e708a1762bf06
Angel Martin <angel.martin@babel.es>
2022-08-06 13:39:05 +0200

    Lectura, creación y edición de categorias. Activar Logger comandos EF en consola de Salida. Deshabilitar nullable a la hora de validar ModelState.IsValid

--- CursoEntityCore/Controllers/CategoriasController.cs
+++ CursoEntityCore/Controllers/CategoriasController.cs
@@ -16,5 +16,112 @@ namespace CursoEntityCore.Controllers
             List<Categoria> listaCategorias = _context.Categoria.ToList();
             return View(listaCategorias);
         }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categoria.Add(categoria);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(); 
+        }
+
+        [HttpGet]
+        public IActionResult CrearMultipleOption2()
+        {
+            List<Categoria> categorias = new List<Categoria>();
+            for (int i = 0; i < 2; i++)
+            {
+                categorias.Add(new Categoria { Name = Guid.NewGuid().ToString() });
+                // _context.Categoria.Add(new Categoria { Name = Guid.NewGuid().ToString() });
+            }
+            _context.Categoria.AddRange(categorias);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult CrearMultipleOption5()
+        {
+            List<Categoria> categorias = new List<Categoria>();
+            for (int i = 0; i < 5; i++)
+            {
+                categorias.Add(new Categoria { Name = Guid.NewGuid().ToString() });
+                // _context.Categoria.Add(new Categoria { Name = Guid.NewGuid().ToString() });
+            }
+            _context.Categoria.AddRange(categorias);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult CrearMultiplesCategorias()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CrearMultipleOpcionFormulario()
+        {
+            string categoriasForm = Request.Form["Name"];
+            var listaCategorias = from val in categoriasForm.Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries) select val;
+
+            List<Categoria> categorias = new List<Categoria>();
+            foreach (var c in listaCategorias)
+            {
+                categorias.Add(new Categoria { Name = c });
+            }
+            _context.Categoria.AddRange(categorias);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return View();
+            }
+            var categoria = _context.Categoria.FirstOrDefault(c => c.Id == id);
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(Categoria categoria)
+        {
+            //if (ModelState.IsValid)
+            //{
+            //    _context.Categoria.Update(categoria);
+            //    _context.SaveChanges();
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //return View(categoria);
+            string categoriaName = Request.Form["Name"];
+            int categoriaId = int.Parse(Request.Form["Id"]);
+
+            if (categoriaId != null && !string.IsNullOrEmpty(categoriaName))
+            {
+                var categoriaToEdit = _context.Categoria.FirstOrDefault(c => c.Id == categoriaId);
+                categoriaToEdit.Name = categoriaName;
+            }
+            else
+            {
+                return View(categoria);
+            }
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- CursoEntityCore/Models/Categoria.cs
+++ CursoEntityCore/Models/Categoria.cs
@@ -1,8 +1,11 @@
-﻿namespace CursoEntityCore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CursoEntityCore.Models
 {
     public class Categoria
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public ICollection<Articulo> Articulos { get; set; }
 
