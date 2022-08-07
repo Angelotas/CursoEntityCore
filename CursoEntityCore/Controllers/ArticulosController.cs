1bfe6d22d3f84ee27ff4cf2fe489ad1e8ba24d7e
Angel Martin <angel.martin@babel.es>
2022-08-07 22:06:04 +0200

    Creación de Artículos con asociación a categoria

---
+++ CursoEntityCore/Controllers/ArticulosController.cs
@@ -0,0 +1,53 @@
+﻿using CursoEntityCore.Datos;
+using CursoEntityCore.Models;
+using CursoEntityCore.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CursoEntityCore.Controllers
+{
+    public class ArticulosController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+        public ArticulosController(ApplicationDbContext contexto)
+        {
+            _context = contexto;
+        }
+        public IActionResult Index()
+        {
+            List<Articulo> listaArticulos = _context.Articulo.ToList();
+            return View(listaArticulos); 
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            ArticuloCategoriaVM articuloCategorias = new ArticuloCategoriaVM();
+            articuloCategorias.ListaCategorias = _context.Categoria.Select(c => new SelectListItem()
+            {
+                Text = c.Name,
+                Value = c.Id.ToString()
+            });
+            return View(articuloCategorias);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Articulo.Add(articulo);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            ArticuloCategoriaVM articuloCategorias = new ArticuloCategoriaVM();
+            articuloCategorias.ListaCategorias = _context.Categoria.Select(c => new SelectListItem()
+            {
+                Text = c.Name,
+                Value = c.Id.ToString()
+            });
+            return View(articuloCategorias);
+        }
+    }
+}
