c2eea08c9938aa97b5f9ca0e6ed59f4dcfbc9e7c
Angel Martin <angel.martin@babel.es>
2022-08-14 20:44:22 +0200

    CRUD Etiquetas

---
+++ CursoEntityCore/Controllers/EtiquetasController.cs
@@ -0,0 +1,73 @@
+﻿using CursoEntityCore.Datos;
+using CursoEntityCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoEntityCore.Controllers
+{
+    public class EtiquetasController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+        public EtiquetasController(ApplicationDbContext contexto)
+        {
+            _context = contexto;
+        }
+        public IActionResult Index()
+        {
+            List<Etiqueta> listaEtiquetas = _context.Etiqueta.ToList();
+            return View(listaEtiquetas);
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Etiqueta etiqueta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Etiqueta.Add(etiqueta);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(); 
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return View();
+            }
+            var etiqueta = _context.Etiqueta.FirstOrDefault(c => c.Etiqueta_Id == id);
+            return View(etiqueta);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(Etiqueta etiqueta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Etiqueta.Update(etiqueta);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(etiqueta);
+        }
+
+        [HttpGet]
+        public IActionResult Borrar(int? id)
+        {
+            var etiquetaToRemove = _context.Etiqueta.FirstOrDefault(a => a.Etiqueta_Id == id);
+            _context.Etiqueta.Remove(etiquetaToRemove);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
